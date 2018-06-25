45e46080cd5a14dbc2c089b56c66e8bf9e1aaadd
Kate Haskell <khaskell@ufl.edu>
2018-06-25 16:18:21 -0400

    Added angular-material

--- app/app.js
+++ app/app.js
@@ -1,4 +1,11 @@
 import angular from 'angular'
+
+import '../node_modules/angular-material/angular-material.css'
+import 'angular-aria'
+import 'angular-animate'
+import 'angular-messages'
+import 'angular-material'
+
 import './app.module.js'
 import './app.controller.js'
 import './book-list/index.js'
